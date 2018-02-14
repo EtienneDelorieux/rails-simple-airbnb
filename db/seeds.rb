4af0316e192af757e61cd89d38bca6e55c130b32
Etienne Delorieux <etienne.delorieux@gmail.com>
2018-02-14 17:03:34 +0100

    Add index view and style on flat cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing database..."
+Flat.destroy_all
+
+puts "Creating datas..."
+10.times do
+  Flat.create!(
+    name: Faker::Hipster.sentence(2),
+    address: Faker::Address.street_address,
+    description: Faker::Hipster.sentence(7),
+    price_per_night: (50..500).to_a.sample,
+    number_of_guests: (1..8).to_a.sample
+  )
+end
